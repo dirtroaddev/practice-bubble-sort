4fe21eef7d7961c0fddf6849d3869f2a1e86389f
dirtroaddev <rongoodwindev@gmail.com>
2024-03-06 16:13:25 -0500

    Building bubble sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,32 @@
 
 function bubbleSort(arr) {
 
-    // Iterate through the array
+  // Iterate through the array
+let swapped = false;
+let i = 0;
+  do {
+    swapped = false;
+    // If the current value is greater than its neighbor to the right
+      // Swap those values
+    for(let i = 0; i < arr.length - 1; i += 1) {
+      if(arr[i] > arr[i + 1]) {
+        [[arr[i]], [arr[i + 1]]] = [[arr[i + 1]], [arr[i]]]
+        swapped = true;
+      }
+    }
+      
+      // Do not move this console.log
+      console.log(arr.join(","));
+    }while(swapped)
+      
 
-      // If the current value is greater than its neighbor to the right
-        // Swap those values
-
-        // Do not move this console.log
-        console.log(arr.join(","));
-
-    // If you get to the end of the array and no swaps have occurred, return
-
-    // Otherwise, repeat from the beginning
+  
+  // If you get to the end of the array and no swaps have occurred, return
+  return arr;
+  // Otherwise, repeat from the beginning
 
 }
+
+
 
 module.exports = bubbleSort;
